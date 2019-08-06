d3f0d655831bce28d964fcf3ec7c4bfe2f440327
milahuminska <48191536+milahuminska@users.noreply.github.com>
2019-08-06 19:53:23 -0400

    Update AppTest.java

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -2,6 +2,9 @@ public class AppTest {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("Hello");
+        System.out.println("Hello,Hello");
+        
+        
   
 
 
