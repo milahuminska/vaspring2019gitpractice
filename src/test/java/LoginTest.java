d79434795b0bd34dbe86e04368ab5e77608b2568
Mila Huminska <huminskaliudmyla@gmail.com>
2019-08-13 19:42:42 -0400

    changed the file

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -2,5 +2,8 @@ public class LoginTest {
     public static void main(String[] args) {
         System.out.println("line 1");
         System.out.println("line 2");
+        System.out.println("line 23");
+        System.out.println("line 24");
+        System.out.println("line 25");
     }
 }
